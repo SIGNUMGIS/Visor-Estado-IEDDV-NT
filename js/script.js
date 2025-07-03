dc2f291660cbf0dac5cfd9dd66500d940ed54528
lfmog <luisfmol19@gmail.com>
2025-07-02 20:47:11 -0500

    Actualización de informacion de avance 20250702

--- js/script.js
+++ js/script.js
@@ -258,10 +258,10 @@ function loadGeoJSON(url, layer, style, labelField, layerType = 'polygon') {
 
 // Load GeoJSON data
 // loadGeoJSON('geojs/Edificacion_Cor_D2.geojson', layers.point, {}, 'PK', 'point');
-loadGeoJSON('geojs/DUCTO_RECORRIDO_IEDDV.geojson', layers.polyline1, styles.polyline1, 'TRM_RML', 'polyline');
+loadGeoJSON('geojs/DUCTO_RECORRIDO_IEDDV_20250702.geojson', layers.polyline1, styles.polyline1, 'TRAMO', 'polyline');
 loadGeoJSON('geojs/DUCTO_ESTRUCTURACION_IEDDV.geojson', layers.polyline2, styles.polyline2, 'TRM_RML', 'polyline');
 loadGeoJSON('geojs/DUCTO_PROGRAMACION_IEDDV.geojson', layers.polyline3, styles.polyline3, 'TRM_RML', 'polyline');
-loadGeoJSON('geojs/DUCTO_RECORRIDO_NT.geojson', layers.polyline4, styles.polyline4, 'TRM_RML', 'polyline');
+loadGeoJSON('geojs/DUCTO_RECORRIDO_NT_20250702.geojson', layers.polyline4, styles.polyline4, 'TRM_RML', 'polyline');
 loadGeoJSON('geojs/DUCTO_ESTRUCTURACION_NT.geojson', layers.polyline5, styles.polyline5, 'TRM_RML', 'polyline');
 loadGeoJSON('geojs/DUCTO_PROGRAMACION_NT.geojson', layers.polyline6, styles.polyline6, 'TRM_RML', 'polyline');
 // loadGeoJSON('geojs/Ducto_Turno4_Adicional.geojson', layers.polyline4, styles.polyline4, 'TRM_RML', 'polyline');
